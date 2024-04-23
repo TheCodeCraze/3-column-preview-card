7051253b6fd3c0460aea8406adef006d431396e9
Tamil Iniyan S <tamiliniyans1999@gmail.com>
2024-04-23 09:54:28 +0530

    Improved accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,9 @@ export const App = () => {
           Choose a sedan for its affordability and excellent fuel economy. Ideal
           for cruising in the city or on your next road trip.
         </p>
-        <button className={styles.cta}>Learn More</button>
+        <button className={styles.cta} aria-label="Click to learn more">
+          Learn More
+        </button>
       </div>
       <div className={styles["col-suv"]}>
         <img
@@ -27,12 +29,14 @@ export const App = () => {
           Take an SUV for its spacious interior, power, and versatility. Perfect
           for your next family vacation and off-road adventures.
         </p>
-        <button className={styles.cta}>Learn More</button>
+        <button className={styles.cta} aria-label="Click to learn more">
+          Learn More
+        </button>
       </div>
       <div className={styles["col-luxury"]}>
         <img
           src="/icon-luxury.svg"
-          alt="Luxury illustration"
+          alt="Luxury car illustration"
           className={styles.icon}
         />
         <h1 className={styles.title}>Luxury</h1>
@@ -40,7 +44,9 @@ export const App = () => {
           Cruise in the best car brands without the bloated prices. Enjoy the
           enhanced comfort of a luxury rental and arrive in style.
         </p>
-        <button className={styles.cta}>Learn More</button>
+        <button className={styles.cta} aria-label="Click to learn more">
+          Learn More
+        </button>
       </div>
     </main>
   );
